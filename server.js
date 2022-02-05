32452fc6f9d57cd9fcde854a69400ce08986669c
jaouasamar <jaoua.samar@gmail.com>
2022-02-06 00:30:50 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require ('express');
+const app=express();
+const fs= require('fs')
+const path= require('path')
+const users=require('./users')
+const PORT= process.env.PORT||5000
+
+/**Middleware **/
+const message=(req,res,next)=>{
+    console.log("hello")
+    next()
+}
+/***Routes***/
+// app.get('/',(req,res)=>{
+//     res.send("<h1>Hello World</h1>");
+ 
+// })
+// app.get('/about',(req,res)=>
+// res.send("<h1>About Page</h1>")
+// )
+// app.get("/users",message,(req,res)=>{
+//     res.send(users)
+// })
+// app.get('/index.html',(req,res)=>
+// fs.readFile('/index.html','utf-8',(err,data)=>
+// {
+//     err?console.log(err):console.log(data);
+// }
+// ))
+app.use(express.static(path.join(__dirname,'public')))
+
+const server=app.listen(PORT,(err)=>err?console.log(err):console.log(`server runnig on port ${PORT}`))
---
+++ users.js
@@ -0,0 +1,18 @@
+const users=[
+    {
+        id:0,
+        fullname:"john",
+        profession:"student"
+    },
+    {
+        id:1,
+        fullname:"michel",
+        profession:"teacher"
+    },
+    {
+        id:2,
+        fullname:"juliette",
+        profession:"manager"
+    }
+]
+module.exports=users;
