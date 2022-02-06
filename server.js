db3b46190930c0c4b3e5019d7be39e02263ad565
jaouasamar <jaoua.samar@gmail.com>
2022-02-06 14:42:12 +0100

    commit

--- server.js
+++ server.js
@@ -18,12 +18,6 @@ const message=(req,res,next)=>{
 // app.get('/about',(req,res)=>
 // res.send("<h1>About Page</h1>")
 // )
-// app.get("/users",message,(req,res)=>{
-//     res.send(users)
-// })
-// app.get("/users",(req,res)=>{
-//     res.json(users);
-// })
 // app.get('/index.html',(req,res)=>
 // fs.readFile('/index.html','utf-8',(err,data)=>
 // {
@@ -32,7 +26,18 @@ const message=(req,res,next)=>{
 // ))
 // app.get('/',(req,res)=>
 // res.sendFile(path.join(__dirname,"public","index.html")))
+/**with API* */
+// app.get("/users",message,(req,res)=>{
+//     res.send(users)
+// })
+app.get("/users",(req,res)=>{
+    res.json(users);
+})
+app.get("/user/:id",(req,res)=>{
+    res.json(users.filter(user=>user.id==req.params.id))
+})
 
-app.use(express.static(path.join(__dirname,'public')))
+
+// app.use(express.static(path.join(__dirname,'public')))
 
 const server=app.listen(PORT,(err)=>err?console.log(err):console.log(`server runnig on port ${PORT}`))
